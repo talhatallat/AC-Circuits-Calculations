7f9c602f6fe847ef390f991fc6286eaac4fd6b45
Talha Tallat <talhatallat0@gmail.com>
2021-10-21 02:46:12 +0100

    Update RL Parallel Header File
    Added multiple options for users to use a calculator efficiently.
    Calcolator lets use find unkown values such as total current(I) or current in inductor(IL) or resistor value(R) and displayes reactance(XL), Impendance(z), current in resistor(IR), current in inductor(IL), Phase angle(0), apparent Power(S), real power(P), reactive Power(Q).

--- RL-Parallel-Circuit-Calculator/RLParallelCircuit.h
+++ RL-Parallel-Circuit-Calculator/RLParallelCircuit.h
@@ -1,3 +1,13 @@
+/*
+	Programmer Name: Talha Tallat 
+	Program Name: RL series circuit
+	Program description: The computer calculates the voltages, power, current, impedance and reactance for a parallel circuit of a resistor and a inductor.  
+			     Calcolator lets use find unkown values such as total current(I) or current in inductor(IL) or resistor value(R) and displayes reactance(XL), Impendance(z),
+			     current in resistor(IR), current in inductor(IL), Phase angle(0), apparent Power(S), real power(P), reactive Power(Q).
+	Date: 11/10/2021 
+	latest update: 21/10/2021
+*/
+
 #ifndef RLPARALLEL
 #define RLPARALLEL
 
@@ -11,53 +21,46 @@ class RLParallel
 {
 public:
 
-	void askUserTheInputValues(float inductance, float frequency, float resistance, float voltage);
+	void askUserTheInputValues(float inductance, float frequency, float resistance, float voltage, float current);
 
 private:
-	int user;
+	char user;
 };
 
 
 //--------------------------------ask user to enter values for RL Circuit------------------------------
-inline void RLParallel  :: askUserTheInputValues (float inductance, float frequency, float resistance, float voltage)
+inline void RLParallel  :: askUserTheInputValues (float inductance, float frequency, float resistance, float voltage, float current)
 {
-		cout << "							RL Parallel circuit calculator							" << endl;
+		cout << "					RL Parallel circuit calculator					" << endl;
 
 		cout << "\nPlease choose the correct following options to use the calculator.\n" <<endl;
 
-		cout << "Enter values to find the total current(I) or current in inductor(IL).\nPress 1 for current(I) OR 2 for current in indoctor(IL) => " ;
+		cout << "Enter values to find the total current(I) or current in inductor(IL) or resistor value(R).\nPress 1 for current(I) OR 2 for current in indoctor(IL) OR 3 for resistor(R) => " ;
 		cin >> user;
 		
-		if(user == 1){
+		if(user == 1 || user == 'I' || user == 'i' ){
 			cout	 <<"\nInductance in Henry (H): " ;
-		cin >> inductance;
-		//cout << "Your inductance is:" << inductance <<"\n" << endl;
+			cin >> inductance;
+			cout << "Frequency in Hertz (Hz): " ;
+			cin >> frequency;
+			cout << "Resistance in ohms: " ;
+			cin >> resistance;
+			cout << "Voltage in volts (v): " ;
+			cin >> voltage;
 
-		cout << "Frequency in Hertz (Hz): " ;
-		cin >> frequency;
-		//cout << "Your frequency is:" << frequency <<"\n" << endl;
-
-		cout << "Resistance in ohms: " ;
-		cin >> resistance;
-		//cout << "Your resistance is:" << resistance <<"\n" << endl;
-
-		cout << "Voltage in volts (v): " ;
-		cin >> voltage;
-		//cout << "Your voltage is: " << voltage <<"\n" << endl;
-
-		//--------------------calculate current for RL circuit------------------------
+		//--------------------calculate to find current in for RL circuit------------------------
 		float XL = (2*3.141592654*frequency*inductance);
-		cout << "\n\n---------Resultat----------\n\nResistance XL: " << XL <<" ohms" << endl;
+		cout << "\n\n---------Result----------\n\nResistance XL: " << XL <<" ohms" << endl;
 		float Z = 1/(sqrt(     ((1/resistance)*(1/resistance))    +     ((1/XL)*(1/XL))       ));
 		cout << "\nImpedance Z: " << Z <<" ohms" << endl;
 		float IR = voltage/resistance ; 
 		cout << "\nCurrent in resistor IR: " << IR <<" A" << endl;
 		float IL = voltage/XL ; 
 		cout << "\nCurrent in indector IL: " << IL <<" A" << endl;
-		float IT= sqrt((IR*IR)+(IL*IL)); 
-		cout << "\nCurrent I: " << IT <<" A" << endl;
+		float I= sqrt((IR*IR)+(IL*IL)); //total current 
+		cout << "\nCurrent I: " << I <<" A" << endl;
 
-		float apparentPower =  voltage * IT;
+		float apparentPower =  voltage * I;
 		cout << "\nApparent power S: " << apparentPower <<" W" << endl;
 
 		float realPower =  voltage * IR;
@@ -70,27 +73,89 @@ inline void RLParallel  :: askUserTheInputValues (float inductance, float freque
 		cout << "\nPhase angle: " << phase <<" Degrees" << endl;
 		}
 
-		else {
+		else if (user == 2 || user == 'IL' || user == 'il'){
+			cout << "Voltage in volts (v): " ;
+			cin >> voltage;
+			cout << "Frequency in Hertz (Hz): " ;
+			cin >> frequency;
+			cout << "Resistance in ohms: " ;
+			cin >> resistance;
+			cout << "Current in Amps (A): " ;
+			cin >> current;
 
-		cout	 <<"\nInductance in Henry (H): " ;
-		cin >> inductance;
-		//cout << "Your inductance is:" << inductance <<"\n" << endl;
+			//--------------------calculate to find current in inductor in RL circuit------------------------
+			float XL = (voltage/current);
+			cout << "\n\n---------Result----------\n\nResistance XL: " << XL <<" ohms" << endl;
+			inductance = XL/(2*3.141592654*frequency);
+			cout << "\nInductance in Hnery(H): " << inductance <<" H" << endl;
+			float Z = 1/(sqrt(     ((1/resistance)*(1/resistance))    +     ((1/XL)*(1/XL))       ));
+			cout << "\nImpedance Z: " << Z <<" ohms" << endl;
+			float IR = voltage/resistance ; 
+			cout << "\nCurrent in resistor IR: " << IR <<" A" << endl;
+			float IL = sqrt( (current*current) - (IR*IR) ) ; //current in inductor
+			cout << "\nCurrent in inductor IL: " << IL <<" A" << endl;
+			float I= sqrt((IR*IR)+(IL*IL)); 
+			cout << "\nCurrent I: " << I <<" A" << endl;
 
-		cout << "Frequency in Hertz (Hz): " ;
-		cin >> frequency;
-		//cout << "Your frequency is:" << frequency <<"\n" << endl;
+			float apparentPower =  voltage * I;
+			cout << "\nApparent power S: " << apparentPower <<" W" << endl;
 
-		cout << "Resistance in ohms: " ;
-		cin >> resistance;
-		//cout << "Your resistance is:" << resistance <<"\n" << endl;
+			float realPower =  voltage * IR;
+			cout << "\nReal power P: " << realPower <<" W" << endl;
 
-		cout << "Voltage in volts (v): " ;
-		cin >> voltage;
-		//cout << "Your voltage is: " << voltage <<"\n" << endl;
-
-		}
-
+			float reactivePower =  voltage * IL;
+			cout << "\nReactive power Q: " << reactivePower <<" W" << endl;
 		
+			float phase = atan(resistance/XL)*180/3.1415;
+			cout << "\nPhase angle: " << phase <<" Degrees" << endl;
+			}
+
+		else if (user == 3 || user == 'R' || user == 'r'){
+			cout << "Voltage in volts (v): " ;
+			cin >> voltage;
+			cout << "Frequency in Hertz (Hz): " ;
+			cin >> frequency;
+			cout	 <<"Inductance in Henry (H): " ;
+			cin >> inductance;
+			cout << "Current in Amps (A): " ;
+			cin >> current;
+
+			//--------------------calculate to find R in RL circuit------------------------
+			float XL = (2*3.141592654*frequency*inductance);
+			cout << "\n\n---------Result----------\n\nResistance XL: " << XL <<" ohms" << endl;
+
+			float IL = voltage/XL ; 
+			cout << "\nCurrent in inductor IL: " << IL <<" A" << endl;
+
+			float IR = sqrt((current*current)-(IL*IL)); 
+			cout << "\nCurrent in resistor IR: " << IR <<" A" << endl;
+
+			resistance = voltage/IR;
+			cout << "\nResistor R: " << resistance <<" ohms" << endl; //R
+
+			float Z = 1/(sqrt(     ((1/resistance)*(1/resistance))    +     ((1/XL)*(1/XL))       ));
+			cout << "\nImpedance Z: " << Z <<" ohms" << endl;
+
+			
+			float I= sqrt((IR*IR)+(IL*IL));
+			cout << "\nCurrent I: " << I <<" A" << endl;
+
+			float apparentPower =  voltage * I;
+			cout << "\nApparent power S: " << apparentPower <<" W" << endl;
+
+			float realPower =  voltage * IR;
+			cout << "\nReal power P: " << realPower <<" W" << endl;
+
+			float reactivePower =  voltage * IL;
+			cout << "\nReactive power Q: " << reactivePower <<" W" << endl;
+		
+			float phase = atan(resistance/XL)*180/3.1415;
+			cout << "\nPhase angle: " << phase <<" Degrees" << endl;
+			}
+
+		else{
+			cout << "Please choose the correct options to use the calculator!" <<endl;
+		}
 		
 }
 
