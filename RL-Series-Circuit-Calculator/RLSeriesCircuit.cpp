e8fdb498dc988f06877bb3f8cd53bdca9fd3c04c
Talha Tallat <talhatallat0@gmail.com>
2021-10-23 23:43:01 +0100

    Final main program file update

--- RL-Series-Circuit-Calculator/RLSeriesCircuit.cpp
+++ RL-Series-Circuit-Calculator/RLSeriesCircuit.cpp
@@ -1,8 +1,11 @@
 /*
 	Programmer Name: Talha Tallat 
 	Program Name: RL series circuit
-	Program description: The computer calculates the voltages, power, current, impedance and reactance for a series circuit of a resistor and a inductor  
-	Date: 11/10/2021
+	Program description: The computer calculates the voltages, power, current, impedance and reactance for a series circuit of a resistor and an inductor. 
+			     Calculator lets user find unknown values such as total current(I) or voltage in inductor(VL) or resistor value(R) and displays reactance(XL), 
+		    	     Impendence(z), voltage in resistor(VR), voltage in inductor(VL), Phase angle(0), apparent Power(S), real power(P), reactive Power(Q).
+	Date: 11/10/2021 
+	latest update: 23/10/2021
 */
 
 #include <stdio.h>
