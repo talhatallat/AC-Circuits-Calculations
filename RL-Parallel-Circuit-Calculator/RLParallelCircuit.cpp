18a2455d9b87fa76377617b682149427056fed07
Talha Tallat <talhatallat0@gmail.com>
2021-10-24 00:00:04 +0100

    Update readme

--- RL-Parallel-Circuit-Calculator/RLParallelCircuit.cpp
+++ RL-Parallel-Circuit-Calculator/RLParallelCircuit.cpp
@@ -1,8 +1,8 @@
 /*
 	Programmer Name: Talha Tallat 
-	Program Name: RL series circuit
+	Program Name: RL Parallel circuit
 	Program description: The computer calculates the voltages, power, current, impedance and reactance for a parallel circuit of a resistor and a inductor.  
-			     Calcolator lets use find unkown values such as total current(I) or current in inductor(IL) or resistor value(R) and displayes reactance(XL), Impendance(z),
+			     Calcolator lets user find unkown values such as total current(I) or current in inductor(IL) or resistor value(R) and displayes reactance(XL), Impendance(z),
 			     current in resistor(IR), current in inductor(IL), Phase angle(0), apparent Power(S), real power(P), reactive Power(Q).
 	Date: 11/10/2021 
 	latest update: 21/10/2021
@@ -13,7 +13,7 @@
 #include <iostream>
 #include <math.h> 
 
-#include "RLParallelCircuit.h"
+#include "RLParalellCircuit.h"
 
 using namespace std;
 
