54fa335d477f6ab4051c38199587c68f9a89a693
Talha Tallat <talhatallat0@gmail.com>
2021-10-23 23:41:13 +0100

    Final program code update
    The computer calculates the voltages, power, current, impedance and reactance for a series circuit of a resistor and an inductor. Calculator lets user find unknown values such as total current(I) or voltage in inductor(VL) or resistor value(R) and displays reactance(XL), Impendence(z), voltage in resistor(VR), voltage in inductor(VL), Phase angle(0), apparent Power(S), real power(P), reactive Power(Q).

--- RL-Series-Circuit-Calculator/RLSeriesCircuitAnalysis.h
+++ RL-Series-Circuit-Calculator/RLSeriesCircuitAnalysis.h
@@ -1,9 +1,18 @@
+/*
+	Programmer Name: Talha Tallat 
+	Program Name: RL series circuit
+	Program description: The computer calculates the voltages, power, current, impedance and reactance for a series circuit of a resistor and an inductor. 
+			     Calculator lets user find unknown values such as total current(I) or voltage in inductor(VL) or resistor value(R) and displays reactance(XL), 
+			     Impendence(z), voltage in resistor(VR), voltage in inductor(VL), Phase angle(0), apparent Power(S), real power(P), reactive Power(Q).
+	Date: 11/10/2021 
+	latest update: 23/10/2021
+*/
 #ifndef RLSERIES
 #define RLSERIES
 
-
 #include <string.h>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -34,33 +43,43 @@ inline void RLSeries  :: askUserTheInputValues (float inductance, float frequenc
 		cout << "Please input to use RL series circuit calculator.\n" 
 			 <<"\nInductance in Henry (H): " ;
 		cin >> inductance;
-		//cout << "Your inductance is:" << inductance <<"\n" << endl;
-
+		
 		cout << "Frequency in Hertz (Hz): " ;
 		cin >> frequency;
-		//cout << "Your frequency is:" << frequency <<"\n" << endl;
 
 		cout << "Resistance in ohms: " ;
 		cin >> resistance;
-		//cout << "Your resistance is:" << resistance <<"\n" << endl;
 
 		cout << "Voltage in volts (v): " ;
 		cin >> voltage;
-		//cout << "Your voltage is: " << voltage <<"\n" << endl;
 
 		//--------------------calculate current for RL circuit------------------------
 		float XL = (2*3.141592654*frequency*inductance);
 		cout << "\nResistance XL: " << XL <<" ohms" << endl;
+
 		float Z = sqrt((resistance*resistance) +(XL*XL));
 		cout << "\nImpedance Z: " << Z <<" ohms" << endl;
+
 		current = voltage/Z ; 
 		cout << "\nCurrent I: " << current <<" A" << endl;
 
-		float apparentPower =  voltage * current;
-		cout << "\nApparent power S: " << apparentPower <<" W" << endl;
-		
+		float VR = current*resistance;
+		cout << "\nVoltage across resistor(VR): " << VR <<" V" << endl;
+
+		float VL = current*XL; 
+		cout << "\nVoltage across Inductor(VL): " << VL <<" V" << endl;
+
 		float phase = atan(XL/resistance)*180/3.1415;
 		cout << "\nPhase angle: " << phase <<" Degrees" << endl;
+
+		float S =  voltage * current;
+		cout << "\nApparent power S: " << S <<" W" << endl;
+
+		float P =  S * cos(phase*(3.141592654/180));
+		cout << "\nReal power P: " << P <<" W" << endl;
+
+		float Q =  S * sin(phase*(3.141592654/180));
+		cout << "\nApparent power Q: " << Q <<" W" << endl;
 		}
 
 		else if (user == '2' || user == 'L' || user == 'l'){
@@ -87,11 +106,65 @@ inline void RLSeries  :: askUserTheInputValues (float inductance, float frequenc
 			inductance = XL/(2*3.141592654*frequency);
 			cout << "\nInductance L: " << inductance <<" H" << endl;
 
-			float apparentPower =  voltage * current;
-			cout << "\nApparent power S: " << apparentPower <<" W" << endl;
-		
+			float VR = current*resistance;
+			cout << "\nVoltage across resistor(VR): " << VR <<" V" << endl;
+
+			float VL = current*XL; 
+			cout << "\nVoltage across Inductor(VL): " << VL <<" V" << endl;
+
 			float phase = atan(XL/resistance)*180/3.1415;
 			cout << "\nPhase angle: " << phase <<" Degrees" << endl;
+
+			float S =  voltage * current;
+			cout << "\nApparent power S: " << S <<" W" << endl;
+
+			float P =  S * cos(phase*(3.141592654/180));
+			cout << "\nReal power P: " << P <<" W" << endl;
+
+			float Q =  S * sin(phase*(3.141592654/180));
+			cout << "\nApparent power Q: " << Q <<" W" << endl;
+		}
+
+
+		else if (user == '3' || user == 'R' || user == 'r'){
+			cout << "Voltage in volts (v): " ;
+			cin >> voltage;
+			cout << "Frequency in Hertz (Hz): " ;
+			cin >> frequency;
+			cout	 <<"Inductance in Henry (H): " ;
+			cin >> inductance;
+			cout << "Current in Amps (A): " ;
+			cin >> current;
+
+			//------------------calculating to find voltage across resistor in RL circuit--------------
+			
+			float XL = (2*3.141592654*frequency*inductance);//(sqrt( (Z*Z)   -  (resistance*resistance)  ));
+			cout << "\nResistance XL: " << XL <<" ohms" << endl;
+
+			float Z = voltage/current;
+			cout << "\nImpedance Z: " << Z <<" ohms" << endl;
+
+			resistance = (sqrt( (Z*Z) -  (XL*XL) ));
+			cout << "\nResistance R: " << resistance <<" ohms" << endl;
+
+			float VR = current*resistance;
+			cout << "\nVoltage across resistor(VR): " << VR <<" V" << endl;
+
+			float VL = current*XL; 
+			cout << "\nVoltage across Inductor(VL): " << VL <<" V" << endl;
+
+			
+			float phase = atan(XL/resistance)*180/3.1415;
+			cout << "\nPhase angle: " << phase <<" Degrees" << endl;
+
+			float S =  voltage * current;
+			cout << "\nApparent power S: " << S <<" W" << endl;
+
+			float P =  S * cos(phase*(3.141592654/180));
+			cout << "\nReal power P: " << P <<" W" << endl;
+
+			float Q =  S * sin(phase*(3.141592654/180));
+			cout << "\nApparent power Q: " << Q <<" W" << endl;
 		}
 		
 		else{
